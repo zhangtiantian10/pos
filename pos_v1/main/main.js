4d5f46477c38c2cb29dcf68b9ed0e72aec00c76b
zhangtiantian10 <975571814@qq.com>
2016-07-18 17:31:29 +0800

    modify inputs to tags

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -1,16 +1,16 @@
 'use strict';
 
-function printReceipt(inputs) {
-  const cartItems = buildCartItems(inputs);
+function printReceipt(tags) {
+  const cartItems = buildCartItems(tags);
 }
 
-function buildCartItems(inputs) {
+function buildCartItems(tags) {
   let cartItems = [];
 
-  for(let input of inputs) {
-    let splitedInput = input.split('-');
-    let barcode = splitedInput[0];
-    let count = parseFloat(splitedInput[1] || 1);
+  for(let tag of tags) {
+    let splitedTag = tag.split('-');
+    let barcode = splitedTag[0];
+    let count = parseFloat(splitedTag[1] || 1);
 
     let cartItem = cartItems.find(cartItem => cartItem.item.barcode === barcode);
 

fd40318db4e010428c930451db5eda6545c076c9
zhangtiantian10 <975571814@qq.com>
2016-07-18 15:33:06 +0800

    write a function is buildCartItems and write a test

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -1,3 +1,46 @@
 'use strict';
 
-//TODO: 请在该文件中实现练习要求并删除此注释
+function printReceipt(inputs) {
+  const cartItems = buildCartItems(inputs);
+}
+
+function buildCartItems(inputs) {
+  let cartItems = [];
+
+  inputs.forEach((input) => {
+    let inputArray = input.split('-');
+    if(inputArray.length != 1){
+      cartItems = searchSameBarcode(cartItems, inputArray[0], parseInt(inputArray[1]));
+    } else {
+      cartItems = searchSameBarcode(cartItems, inputArray[0], 1);
+    }
+  });
+
+  return cartItems;
+}
+
+function searchSameBarcode(cartItems, barcode, count) {
+  let item = searchBarcodeInAllItems(barcode);
+
+  let findBarcode = cartItems.find((cartItem) => {
+    return barcode === cartItem.item.barcode;
+  });
+
+  if(findBarcode) {
+    cartItems[cartItems.indexOf(findBarcode)].count += count;
+  } else {
+    cartItems.push({item: item , count: count});
+  }
+
+  return cartItems;
+}
+
+
+function searchBarcodeInAllItems(barcode) {
+  const allItems = loadAllItems();
+
+  return allItems.find((item) => {
+    return item.barcode === barcode;
+  });
+
+}
--- pos_v1/test/main-test.js
+++ pos_v1/test/main-test.js
@@ -17,6 +17,36 @@ describe('pos', () => {
     ];
   });
 
+  it('buildCartItems', () => {
+    const cartItems = [{
+      item:{
+        barcode: 'ITEM000001',
+        name: '雪碧',
+        unit: '瓶',
+        price: 3.00
+      },
+      count: 5
+    }, {
+      item: {
+        barcode: 'ITEM000003',
+        name: '荔枝',
+        unit: '斤',
+        price: 15.00
+      },
+      count: 2
+    },{
+      item:{
+        barcode: 'ITEM000005',
+        name: '方便面',
+        unit: '袋',
+        price: 4.50
+      },
+      count: 3
+    }];
+
+    expect(buildCartItems(inputs)).toEqual(cartItems);
+  });
+
   it('should print correct text', () => {
 
     spyOn(console, 'log');
